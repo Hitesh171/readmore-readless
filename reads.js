a68aa25933a26e90b055938c784fae31761ec196
Hitesh171 <127680967+Hitesh171@users.noreply.github.com>
2023-03-28 12:43:53 +0530

    added reads.js

---
+++ reads.js
@@ -0,0 +1,17 @@
+const readMoreLessList = Array.from(document.querySelectorAll('.read-more-less'));
+
+readMoreLessList.forEach((readMoreLess) => {
+	readMoreLess.addEventListener('click', () => {
+
+		const hiddenContent = readMoreLess.parentElement.querySelector('.hidden-content');
+
+		if (hiddenContent.classList.contains('hide')){
+			hiddenContent.classList.remove('hide');
+			readMoreLess.textContent = "Read Less";
+		}
+		else {
+			hiddenContent.classList.add('hide');
+			readMoreLess.textContent = "Read More";
+		}
+	});
+});
